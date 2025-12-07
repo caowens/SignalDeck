96d0506e4a6d5a5a5fc5b47c3b025df323e19f30
caowens <caowens555@yahoo.com>
2025-12-06 23:04:00 -0800

    Add Metric DTOs

---
+++ backend/SignalDeck.Application/DTOs/Metric/MetricDto.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalDeck.Application.DTOs.Metric
+{
+    public class MetricDto
+    {
+        public int Id { get; set; }
+        public int ApplicationId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double Value { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string? PropertiesAsJson { get; set; }
+    }
+}
