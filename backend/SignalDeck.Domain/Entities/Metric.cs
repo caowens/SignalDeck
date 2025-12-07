136f2e68c8c764b6e4090612c25c9a9a61533928
caowens <caowens555@yahoo.com>
2025-12-06 22:37:42 -0800

    Make sure Metric extends BaseEntity

--- backend/SignalDeck.Domain/Entities/Metric.cs
+++ backend/SignalDeck.Domain/Entities/Metric.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SignalDeck.Domain.Common;
 
 namespace SignalDeck.Domain.Entities
 {
-    public class Metric
+    public class Metric : BaseEntity
     {
         public int ApplicationId { get; set; }
         public Application Application { get; set; } = default!;
