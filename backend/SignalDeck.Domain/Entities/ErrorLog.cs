8bd9c7a15ba6eca149d3e8d40fbc3cba65bb9404
caowens <caowens555@yahoo.com>
2025-12-06 22:25:58 -0800

    Remove uneccessary Application entity conversion

--- backend/SignalDeck.Domain/Entities/ErrorLog.cs
+++ backend/SignalDeck.Domain/Entities/ErrorLog.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SignalDeck.Domain.Common;
-using ApplicationEntity = SignalDeck.Domain.Entities.Application;
 
 namespace SignalDeck.Domain.Entities
 {
     public class ErrorLog : BaseEntity
     {
         public int ApplicationId { get; set; }
-        public ApplicationEntity Application { get; set; } = default!;
+        public Application Application { get; set; } = default!;
 
         public string Message { get; set; } = string.Empty;
         public string? StackTrace { get; set; }
