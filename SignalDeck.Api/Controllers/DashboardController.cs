9f3c06a65b3d35c8760639ee78792fd8659bc92f
caowens <caowens555@yahoo.com>
2026-01-13 10:33:59 -0800

    Make new controllers for new simplified Api structure

--- SignalDeck.Api/Controllers/DashboardController.cs
+++ SignalDeck.Api/Controllers/DashboardController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SignalDeck.Application.Services.Analytics;
+using SignalDeck.Api.Data;
 
 namespace SignalDeck.Api.Controllers
 {
@@ -11,17 +11,17 @@ namespace SignalDeck.Api.Controllers
     [Route("api/v1/[controller]")]
     public class DashboardController : ControllerBase
     {
-        private readonly IAnalyticsService _analyticsService;
-        public DashboardController(IAnalyticsService analyticsService)
+        private readonly SignalDeckDbContext _context;
+        public DashboardController(SignalDeckDbContext context)
         {
-            _analyticsService = analyticsService;
+            _context = context;
         }
         
         [HttpGet("summary/{appId}")]
         public async Task<IActionResult> GetDashboardSummary(Guid appId)
         {
-            var summary = await _analyticsService.GetSummaryAsync(appId);
-            return Ok(summary);
+            // var summary = await _analyticsService.GetSummaryAsync(appId);
+            return Ok("Placeholder summary");
         }
     }
 }
