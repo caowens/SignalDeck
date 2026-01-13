5997a560dab394b99bf3633d6da1bdcc64f4a450
caowens <caowens555@yahoo.com>
2026-01-13 09:04:08 -0800

    Create Signal Entity

---
+++ backend/SignalDeck.Domain/Entities/SignalEntity.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using SignalDeck.Domain.Common;
+
+namespace SignalDeck.Domain.Entities
+{
+    public class SignalEntity
+    {
+        // Database-specific fields
+        [Key]
+        public int InternalId { get; set; }
+        [Required]
+        public int ApplicationId { get; set; }
+        public Application Application { get; set; } = default!;
+        public DateTime ServerRecievedAt { get; set; } = DateTime.UtcNow;
+
+        // Data mirrored from the event
+        public Guid ExternalId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public SignalSeverity Severity { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public bool IsError => Severity >= SignalSeverity.Error;
+        public string? Message { get; set; }
+        public string? StackTrace { get; set; }
+        public DateTime EventTimestamp { get; set; }
+        public Dictionary<string, string> Properties { get; set; } = new();
+    }
+}
---
+++ backend/SignalDeck.Domain/Entities/SignalSeverity.cs
@@ -0,0 +1,12 @@
+namespace SignalDeck.Domain.Entities
+{
+    public enum SignalSeverity
+    {
+        Trace = 0,
+        Debug = 1,
+        Info = 2,
+        Warning = 3,
+        Error = 4,
+        Critical = 5
+    }
+}
